82d45304da3a80ce6278e5aec05a54822fd2c9bf
sem-antic <sem-antic@ya.ru>
2023-03-02 18:11:01 +0300

    added key_formatter as constructor param

--- xml_to_dict/xml_to_dict.py
+++ xml_to_dict/xml_to_dict.py
@@ -1,11 +1,21 @@
 import xml.etree.ElementTree as ET, re
 from collections import defaultdict
+from collections.abc import Callable
 from typing import Union
 
 
 class XMLtoDict(object):
 
-    def parse(self, xml: str):
+    """
+    Parameters
+    ----------
+    key_formatter : Callable[[str], str], default=None
+        Callback for formatting tag names into resulting dictionary keys
+    """
+    def __init__(self, key_formatter: Callable[[str], str] = None):
+        self.key_formatter = key_formatter
+
+    def parse(self, xml: str, ):
         return self.__to_dict(ET.fromstring(xml))
     
     def value_from_nest(self, pattern: str, nest: Union[dict, str]):
@@ -17,25 +27,29 @@ class XMLtoDict(object):
             if type(v) is dict:
                 return self.value_from_nest(pattern, v)
 
-    def __to_dict(self, t: str):
-        d = {t.tag: {} if t.attrib else None}
+    def __to_dict(self, t: ET.Element):
+        key = t.tag
+        if self.key_formatter:
+            key = self.key_formatter(key)
+
+        d = {key: {} if t.attrib else None}
         children = list(t)
         if children:
             dd = defaultdict(list)
             for dc in map(self.__to_dict, children):
                 for k, v in dc.items():
                     dd[k].append(v)
-            d = {t.tag: {
+            d = {key: {
                     k:v[0] if len(v) == 1 \
                         else v for k, v in dd.items()
             }}
         if t.attrib:
-            d[t.tag].update(('@' + k, v) for k, v in t.attrib.items())
+            d[key].update(('@' + k, v) for k, v in t.attrib.items())
         if t.text:
             text = t.text.strip()
             if children or t.attrib:
                 if text:
-                    d[t.tag]['#text'] = text
+                    d[key]['#text'] = text
             else:
-                d[t.tag] = text
+                d[key] = text
         return d
